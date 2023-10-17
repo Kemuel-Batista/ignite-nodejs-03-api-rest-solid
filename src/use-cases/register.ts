d230f1962fe1b87aa228d029285516bc365a17a9
Kemuel Batista <kemuellima20@gmail.com>
2023-10-16 21:10:12 -0300

    heat: repositoty and use cases of users register

---
+++ src/use-cases/register.ts
@@ -0,0 +1,25 @@
+import { IUsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface IRegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({ name, email, password }: IRegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({ name, email, password_hash })
+  }
+}
